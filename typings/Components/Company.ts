0f55774a39f5d6897e6c5fec9e0f5c0aee5a9431
romdim <rom-dim@hotmail.com>
2021-01-03 02:19:29 +0100

    Latest export

--- typings/Components/Company.ts
+++ typings/Components/Company.ts
@@ -1,10 +1,10 @@
 import type CarouselItem from './CarouselItem';
+import type Columns from './Columns';
 
 type Company = {
   name: string,
   nameC?: string,
-  start: number,
-  span: number,
+  columns: Columns,
   carousel: CarouselItem[],
   employerText: string,
   roleText: string,
