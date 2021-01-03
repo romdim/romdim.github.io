79d816e3eb617ebb61e0a1ee22733bcb44bb723e
romdim <rom-dim@hotmail.com>
2021-01-03 03:23:35 +0100

    Header-Footer less columns responsively

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,21 +8,23 @@
   }
 </style>
 
-<footer class="relative mt-25 2xl:mt-30 py-13 px-0 bg-none font-cursive text-2xl">
-  <p class="pb-7">KvK-nummer: 75633264</p>
-  <p class="pb-7">BTW-nummer: NL860346432B01</p>
-  <div class="flex">
-    <a href="https://github.com/romdim" target="_blank" rel="noopener noreferrer">
-      <img class="h-11 pr-11" src="img/contact/github.svg" alt="Github logo for romdim">
-    </a>
-    <a href="https://linkedin.com/in/romdim" target="_blank" rel="noopener noreferrer">
-      <img class="h-11 pr-11" src="img/contact/linkedin.svg" alt="LinkedIn logo for romdim">
-    </a>
-    <a href="mailto:rom-dim@hotmail.com?subject=Coming%20from%20site" target="_blank" rel="noopener noreferrer">
-      <img class="h-11 pr-11" src="img/contact/mail.svg" alt="romdim e-mail">
-    </a>
+<footer class="mt-25 2xl:mt-30 2xl:mb-20 py-13 px-0 bg-none font-cursive text-2xl grid grid-cols-12">
+  <div class="relative col-start-2 3xl:col-start-1 col-span-10 3xl:col-span-12">
+    <p class="pb-7">KvK-nummer: 75633264</p>
+    <p class="pb-7">BTW-nummer: NL860346432B01</p>
+    <div class="flex">
+      <a href="https://github.com/romdim" target="_blank" rel="noopener noreferrer">
+        <img class="h-11 pr-11" src="img/contact/github.svg" alt="Github logo for romdim">
+      </a>
+      <a href="https://linkedin.com/in/romdim" target="_blank" rel="noopener noreferrer">
+        <img class="h-11 pr-11" src="img/contact/linkedin.svg" alt="LinkedIn logo for romdim">
+      </a>
+      <a href="mailto:rom-dim@hotmail.com?subject=Coming%20from%20site" target="_blank" rel="noopener noreferrer">
+        <img class="h-11 pr-11" src="img/contact/mail.svg" alt="romdim e-mail">
+      </a>
+      <a href="{page}#top" class="absolute right-0 transform origin-center -translate-y-1/2 -scale-y-100 -rotate-90" aria-label="Scroll to Top">
+        <svg xmlns="http://www.w3.org/2000/svg" width="80px" viewBox="0 0 297.7976 174.5396"><g transform="translate(-96.7857 -49.9167)" fill="none" stroke="#000" stroke-width="4" stroke-linecap="round"><path d="M183.7857 221.4563c-46.3919 0-84-37.729-84-84.2698 0-46.541 37.6081-84.2698 84-84.2698 31.0636 0 58.1889 16.9157 72.7226 42.066"/><path d="M146.1578 136.8582c0-22.7347 17.9938-41.1649 40.1903-41.1649 6.6187 0 12.8637 1.6387 18.3694 4.5416a39.9535 39.9535 0 013.8847 2.3398 40.6033 40.6033 0 016.1648 5.1755c3.0032 3.076 5.5333 6.6366 7.469 10.5574"/><path d="M185.2083 52.9167h185.2084c11.7263 0 21.1666 9.4403 21.1666 21.1666 0 11.7264-9.4403 21.1667-21.1666 21.1667H256.8892"/><rect width="84.6667" height="42.3333" x="222.25" y="137.5833" ry="21.1466"/><rect width="84.6667" height="41.5396" x="222.25" y="179.9167" ry="20.7698"/><rect width="84.6667" height="42.3333" x="222.25" y="95.25" ry="21.1466"/><path d="M183.7857 221.4563h64.5396"/></g></svg>
+      </a>
+    </div>
   </div>
-  <a href="{page}#top" class="absolute top-1/2 right-0 transform origin-center -translate-y-1/2 -scale-y-100 -rotate-90" aria-label="Scroll to Top">
-    <svg xmlns="http://www.w3.org/2000/svg" width="80px" viewBox="0 0 297.7976 174.5396"><g transform="translate(-96.7857 -49.9167)" fill="none" stroke="#000" stroke-width="4" stroke-linecap="round"><path d="M183.7857 221.4563c-46.3919 0-84-37.729-84-84.2698 0-46.541 37.6081-84.2698 84-84.2698 31.0636 0 58.1889 16.9157 72.7226 42.066"/><path d="M146.1578 136.8582c0-22.7347 17.9938-41.1649 40.1903-41.1649 6.6187 0 12.8637 1.6387 18.3694 4.5416a39.9535 39.9535 0 013.8847 2.3398 40.6033 40.6033 0 016.1648 5.1755c3.0032 3.076 5.5333 6.6366 7.469 10.5574"/><path d="M185.2083 52.9167h185.2084c11.7263 0 21.1666 9.4403 21.1666 21.1666 0 11.7264-9.4403 21.1667-21.1666 21.1667H256.8892"/><rect width="84.6667" height="42.3333" x="222.25" y="137.5833" ry="21.1466"/><rect width="84.6667" height="41.5396" x="222.25" y="179.9167" ry="20.7698"/><rect width="84.6667" height="42.3333" x="222.25" y="95.25" ry="21.1466"/><path d="M183.7857 221.4563h64.5396"/></g></svg>
-  </a>
 </footer>
