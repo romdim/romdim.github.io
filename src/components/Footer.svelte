0f9c47c1fac645a47c153ecd3951fbf93bc2e404
romdim <rom-dim@hotmail.com>
2021-01-19 02:29:54 +0100

    Replaced pr to mr for contact at footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -27,16 +27,15 @@
     <p class="pb-4 md:pb-7.5">BTW - NL860346432B01</p>
     <div class="flex">
       <a href="https://github.com/romdim" target="_blank" rel="noopener noreferrer">
-        <img class="h-4 md:h-5.5 lg:h-7 3.1xl:h-8.5 pr-3.5 md:pr-5 lg:pr-7.5 3.1xl:pr-10" src="img/contact/github.svg" alt="Github logo for romdim">
+        <img class="h-4 md:h-5.5 lg:h-7 3.1xl:h-8.5 mr-3.5 md:mr-5 lg:mr-7.5 3.1xl:mr-10" src="img/contact/github.svg" alt="Github logo for romdim">
       </a>
       <a href="https://linkedin.com/in/romdim" target="_blank" rel="noopener noreferrer">
-        <img class="h-4 md:h-5.5 lg:h-7 3.1xl:h-8.5 pr-3.5 md:pr-5 lg:pr-7.5 3.1xl:pr-10" src="img/contact/linkedin.svg" alt="LinkedIn logo for romdim">
+        <img class="h-4 md:h-5.5 lg:h-7 3.1xl:h-8.5 mr-3.5 md:mr-5 lg:mr-7.5 3.1xl:mr-10" src="img/contact/linkedin.svg" alt="LinkedIn logo for romdim">
       </a>
       <a href="mailto:rom-dim@hotmail.com?subject=Coming%20from%20site" target="_blank" rel="noopener noreferrer">
-        <img class="h-4 md:h-5.5 lg:h-7 3.1xl:h-8.5 pr-3.5 md:pr-5 lg:pr-7.5 3.1xl:pr-10" src="img/contact/mail.svg" alt="romdim e-mail">
+        <img class="h-4 md:h-5.5 lg:h-7 3.1xl:h-8.5 mr-3.5 md:mr-5 lg:mr-7.5 3.1xl:mr-10" src="img/contact/mail.svg" alt="romdim e-mail">
       </a>
       <a href="{page}#top" class="absolute bottom-0 right-0 w-10 md:w-15 lg:w-20 transform origin-center -translate-y-1/3 -scale-y-100 -rotate-90 group hand-up" aria-label="Scroll to Top">
-        <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 297.7976 174.5396"><g transform="translate(-96.7857 -49.9167)" fill="none" stroke="#000" stroke-width="4" stroke-linecap="round"><path d="M183.7857 221.4563c-46.3919 0-84-37.729-84-84.2698 0-46.541 37.6081-84.2698 84-84.2698 31.0636 0 58.1889 16.9157 72.7226 42.066"/><path d="M146.1578 136.8582c0-22.7347 17.9938-41.1649 40.1903-41.1649 6.6187 0 12.8637 1.6387 18.3694 4.5416a39.9535 39.9535 0 013.8847 2.3398 40.6033 40.6033 0 016.1648 5.1755c3.0032 3.076 5.5333 6.6366 7.469 10.5574"/><path d="M185.2083 52.9167h185.2084c11.7263 0 21.1666 9.4403 21.1666 21.1666 0 11.7264-9.4403 21.1667-21.1666 21.1667H256.8892"/><rect width="84.6667" height="42.3333" x="222.25" y="137.5833" ry="21.1466"/><rect width="84.6667" height="41.5396" x="222.25" y="179.9167" ry="20.7698"/><rect width="84.6667" height="42.3333" x="222.25" y="95.25" ry="21.1466"/><path d="M183.7857 221.4563h64.5396"/></g></svg> -->
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 297.7976 174.5396"><g transform="translate(-96.7857 -49.9167)" fill="none" stroke="#000" stroke-width="6" stroke-linecap="round"><path d="M183.7857 221.4563c-46.3919 0-84-37.729-84-84.2698 0-46.541 37.6081-84.2698 84-84.2698 31.0636 0 58.1889 16.9157 72.7226 42.066"/><path d="M146.1578 136.8582c0-22.7347 17.9938-41.1649 40.1903-41.1649 6.6187 0 12.8637 1.6387 18.3694 4.5416a39.9535 39.9535 0 013.8847 2.3398 40.6033 40.6033 0 016.1648 5.1755c3.0032 3.076 5.5333 6.6366 7.469 10.5574"/><path d="M185.2083 52.9167h185.2084c11.7263 0 21.1666 9.4403 21.1666 21.1666 0 11.7264-9.4403 21.1667-21.1666 21.1667H256.8892"/><rect width="84.6667" height="42.3333" x="222.25" y="137.5833" ry="21.1466"/><rect width="84.6667" height="41.5396" x="222.25" y="179.9167" ry="20.7698"/><rect width="84.6667" height="42.3333" x="222.25" y="95.25" ry="21.1466"/><path d="M183.7857 221.4563h64.5396"/></g><ellipse class="stroke-current text-transparent group-hover:text-black" fill="none" stroke-width="6" cx="89.509709" cy="85.153151" rx="20.613924" ry="20.785707"/><circle class="fill-current text-transparent active-text-black" cx="89.673386" cy="85.314531" r="10.491192"/></svg>
       </a>
     </div>
