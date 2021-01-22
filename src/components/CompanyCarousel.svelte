108a8dab0c7faccca2139739d0e6a2ecf8bcd576
romdim <rom-dim@hotmail.com>
2021-01-22 03:59:38 +0100

    Progressive images ftw

--- src/components/CompanyCarousel.svelte
+++ src/components/CompanyCarousel.svelte
@@ -1,11 +1,15 @@
 <script>
   import { onMount } from 'svelte';
 
+  import Image from "svelte-progressive-image";
+  
   import type CarouselItem from '../../typings/Components/CarouselItem';
+  import type Columns from 'typings/Components/Columns';
 
   export let name: string;
   export let nameC: string = name.charAt(0).toUpperCase() + name.slice(1);
   export let carousel: CarouselItem[];
+  export let columns: Columns;
   
   const widthBuilder = (prefix: string, width: string): string => {
     return width ? ` ${prefix}:w-${width}` : '';
@@ -13,6 +17,26 @@
 
   let logoWidth: string = carousel[0].width ? `w-${carousel[0].width.default}` + widthBuilder('md', carousel[0].width.md) + widthBuilder('lg', carousel[0].width.lg) + widthBuilder('xl', carousel[0].width.xl) + widthBuilder('2xl', carousel[0].width['2xl']) + widthBuilder('3xl', carousel[0].width['3xl']) + widthBuilder('3.1xl', carousel[0].width['3.1xl']) : 'w-1/2';
   
+  const widths: number[] = [
+    640,
+    768,
+    1024,
+    1280,
+    1440,
+    1920
+  ];
+  const defaultWidth: number = 1920;
+  const ratio: number = columns.span/12;
+  const sizes: [number, number][] = [
+    [640, 1],
+    [768, 1],
+    [1024, 1],
+    [1280, ratio],
+    [1440, ratio],
+    [1920, columns['3xl'] ? columns['3xl'].span/12 : ratio]
+  ];
+  const largestImageSize: number = 1920;
+
   let list: HTMLUListElement;
   let inputs: HTMLCollectionOf<HTMLInputElement>;
 
@@ -156,6 +180,7 @@
       <li class="{item.type === 'logo' ? 'relative ' : ''}transition-opacity duration-1000 opacity-0 z-auto">
         {#if item.type === 'logo'}
           <div class="aspect-w-16 aspect-h-9 bg-{name} {item.border ? `border border-solid border-${name}-secondary` : ''}"></div>
+          <!-- TODO Alt -->
           <img class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 {logoWidth} h-auto" src="img/companies/{nameC}/{nameC}.{item.format ? item.format : 'svg'}" alt="">
         {:else if item.type === 'video'}
           <video width="100%" controls muted={true}>
@@ -163,7 +188,15 @@
             Your browser does not support the video tag.
           </video>
         {:else}
-          <img src="img/companies/{nameC}/{nameC}{item.name ? `-${item.name}` : ''}.png" alt="">
+        <!-- TODO Alt images -->
+          <Image
+            name={`img/companies/${nameC}/${nameC}${item.name ? `-${item.name}` : ''}`}
+            {widths}
+            {defaultWidth}
+            {sizes}
+            {largestImageSize}
+            alt=""
+          />
         {/if}
       </li>
     {/each}
