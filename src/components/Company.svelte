ecdd3b8c032ae1500dd9c9d4ce5d0516260fc89e
romdim <rom-dim@hotmail.com>
2021-01-25 19:56:43 +0100

    Fixing parameter warnings (still like previous passing more)

--- src/components/Company.svelte
+++ src/components/Company.svelte
@@ -11,6 +11,16 @@
 </script>
 
 <div class="col-span-full lg:col-start-{columns.start} lg:col-span-{columns.span} {columns['3xl'] ? `3xl:col-start-${columns['3xl'].start} 3xl:col-span-${columns['3xl'].span} ` : '' }mt-10 md:mt-12.5 lg:mt-15 3xl:mt-17 company-{name}">
-  <CompanyCarousel {...company} />
-  <CompanyInfo {...company} />
+  <CompanyCarousel
+    name={company.name}
+    nameC={company.nameC}
+    carousel={company.carousel}
+    columns={company.columns}
+  />
+  <CompanyInfo
+    name={company.name}
+    employerText={company.employerText}
+    roleText={company.roleText}
+    tools={company.tools}
+  />
 </div>
